22b6b50c581f8114dd30b1482fc3658cf6225ebb
Mizrraim <garcialopezmizrraim05@gmail.com>
2019-07-29 23:31:45 -0500

    tercer commit agregando codigo a la clase database

--- database.php
+++ database.php
@@ -1,3 +1,12 @@
 <?php
+	$conexion = new mysqli("localhost","root","garcialopez","jamaica");
 
+	if ($conexion) {
+        # code...
+	}else{
+		echo'<script type="text/javascript"> 
+    	alert("Error de conexion");
+    	window.location.href="indexInsertar.html";
+    </script>';
+	}
 ?>
